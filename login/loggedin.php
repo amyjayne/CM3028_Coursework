454ce1e5f3dbf156d165bf467090a82e3f3317bd
amy <amyjayne-96@hotmail.co.uk>
2016-11-29 16:51:14 +0000

    Initial commit

--- login/loggedin.php
+++ login/loggedin.php
@@ -11,7 +11,7 @@ displayAccessLevelInformation($accesslevel);
 function displayAccessLevelInformation($accessLevel)
 {
     if ($accessLevel == "standarduser") {
-        echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user</p>";
+        echo "<p style = \"background-color: lightgreen\">You are currently</p>";
     }
     elseif ($accessLevel == "root") {
         echo "<p<p style = \"background-color: red\">You are currently logged in as a root user</p>";
