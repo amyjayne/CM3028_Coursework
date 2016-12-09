cc00ecc788ad44add377061d1a1b58d7e7c825b9
Jaimie <peanutflea@gmail.com>
2016-12-09 14:23:15 +0000

    login on homepage

--- editclubpage.php
+++ editclubpage.php
@@ -16,6 +16,7 @@ if (isset($_SESSION['username'])) //SESSION DOES EXIST
         <?
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
         include ('dbconnect.php');
+        $clubID = str_replace(' ', '-', $_POST["clubName"]);
         $clubName = $_POST["clubName"];
         $description = $_POST["description"];
         $sql = "INSERT INTO club (clubName, description) VALUES ('". $clubName ."', '".$description."')";
