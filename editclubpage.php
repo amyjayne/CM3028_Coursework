364cdaf915db5c34fa754ef11f229e1d17f4e634
Jaimie <peanutflea@gmail.com>
2016-12-09 16:22:31 +0000

    login on homepage

--- editclubpage.php
+++ editclubpage.php
@@ -9,6 +9,7 @@ if (isset($_SESSION['username'])) //SESSION DOES EXIST
             <script>tinymce.init({selector: 'textarea'});</script>
             <form action="editclubpage.php" method="post">
                 <input type="text" name="clubName" placeholder="Club Name">
+                <input type="hidden" value="1" name="accessID">
                 <textarea name="description"></textarea>
                 <input type="submit">
             </form>
@@ -17,9 +18,10 @@ if (isset($_SESSION['username'])) //SESSION DOES EXIST
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
         include ('dbconnect.php');
         $clubID = str_replace(' ', '-', $_POST["clubName"]);
+        $accessID = $_POST['accessID'];
         $clubName = $_POST["clubName"];
         $description = $_POST["description"];
-        $sql = "INSERT INTO club (clubID, clubName, description) VALUES ('". $clubID ."', '". $clubName ."', '".$description."')";
+        $sql = "INSERT INTO club (clubID, clubName, description) VALUES ('". $clubID ."', '". $accessID ."', '". $clubName ."', '".$description."')";
         if (mysqli_query($db, $sql)) {
         } else {
             echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
