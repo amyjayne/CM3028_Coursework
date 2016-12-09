87757628d803d01d9550617c62d7d7ac5a7e1093
Jaimie <peanutflea@gmail.com>
2016-12-09 14:20:30 +0000

    login on homepage

---
+++ editclubpage.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if (isset($_SESSION['username'])) //SESSION DOES EXIST
+{
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        ?>
+        <main>
+            <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+            <script>tinymce.init({selector: 'textarea'});</script>
+            <form action="editclubpage.php" method="post">
+                <input type="text" name="clubName" placeholder="Club Name">
+                <textarea name="description"></textarea>
+                <input type="submit">
+            </form>
+        </main>
+        <?
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        include ('dbconnect.php');
+        $clubName = $_POST["clubName"];
+        $description = $_POST["description"];
+        $sql = "INSERT INTO club (clubName, description) VALUES ('". $clubName ."', '".$description."')";
+        if (mysqli_query($db, $sql)) {
+        } else {
+            echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
+        }
+        header("signedin.php");
+    }
+//test
+} else {
+    header("location:editclubpage.php");
+}
+?>
--- signedin.php
+++ signedin.php
@@ -10,7 +10,7 @@
 <div class="header">
     <div class="home-menu pure-menu pure-menu-fixed">
         <ul class="pure-menu-list">
-            <li><a href ="homepage.php">Home</a></li>
+            <li><a href ="editclubpage.php">Home</a></li>
             <li><a href ="clubs.html">Clubs</a></li>
             <li><a href ="health.html">Health and Fitness</a></li>
             <li><a href ="blog.html">Blog</a></li>
