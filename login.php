43b969dee673afd4280a4d23a92f5a1eb9a3f933
Amy <amyjayne-96@hotmail.co.uk>
2016-12-12 18:38:56 +0000

    clubs

--- login.php
+++ login.php
@@ -60,16 +60,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 }
 ?>
 
-<?php
-
-$time=$_SERVER['REQUEST_TIME'];
+<!-- $time=$_SERVER['REQUEST_TIME'];
 $timeout_duration = 1000;
 if (isset($_session['LAST_ACTIVITY']) && ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) [
     session_unset();
 session_destroy();
 session_start();}
-$_SESSION['LAST_ACTIVITY']=$time
-
-?>
+$_SESSION['LAST_ACTIVITY']=$time -->
 
 </body>
