9be3f2063e4b636f5cd72024bf77b487c7cfe38c
Amy <amyjayne-96@hotmail.co.uk>
2016-12-12 12:29:10 +0000

    clubs

--- signedin.php
+++ signedin.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['username']))
             <li><a href ="signedin.php">Home</a></li>
             <li><a href ="clubspage.php">Clubs</a></li>
             <li><a href ="health.php">Health and Fitness</a></li>
-            <li><a href ="contact.php">Contact</a></li>
+            <li><a href ="logout.php">Log Out</a></li>
         </ul>
     </div>
 </div>
