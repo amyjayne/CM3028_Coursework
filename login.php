0af4f58f3dca9fc361a574915f295112db08d944
Amy <amyjayne-96@hotmail.co.uk>
2016-12-12 14:54:43 +0000

    clubs

--- login.php
+++ login.php
@@ -7,6 +7,16 @@
 </head>
 <body>
 
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-fixed">
+        <ul class="pure-menu-list">
+            <li><a href ="index.php">Home</a></li>
+            <li><a href ="clubs.php">Clubs</a></li>
+            <li><a href ="health.php">Health and Fitness</a></li>
+        </ul>
+    </div>
+</div>
+
 <div class="login">
 <?php
 define('CSS_PATH', 'template/css/');
