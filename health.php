5398c10c20ac134aeb14b2f7e742f749586caa19
Amy <amyjayne-96@hotmail.co.uk>
2016-12-12 18:32:58 +0000

    clubs

--- health.php
+++ health.php
@@ -19,19 +19,17 @@
 
     <div id="content">
         <p> Health and wellbeing Information</p>
-        <p>
+
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
         
         <img src="images/exercise.jpg" id="image" alt="image">
-        
-        <p>
+
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+
       
             <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=en.uk%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;src=a8riso7s8bfkj460lkcisfnsfg%40group.calendar.google.com&amp;color=%23853104&amp;ctz=America%2FNew_York" style="border-width:0" width="50%" height="350" frameborder="0" scrolling="no"></iframe>
     </div>
