17fadc9abeb72b3ce7e242ae5b7c04bb839fec63
Jaimie <peanutflea@gmail.com>
2016-12-12 11:42:43 +0000

    login on homepage

--- club1.php
+++ club1.php
@@ -5,10 +5,11 @@ $sql = "SELECT * FROM club";
 $result = $db->query($sql);
 while($row = $result->fetch_array())
 {
+    $clubID = $row['clubID'];
     $clubName = $row['clubName'];
     $description = $row['description'];
 
-    echo "<li><a href='club/{$articleID}'>{$articleName}</a> by {$articleAuthor}</li>";
+    echo "<li><a href='club/{$clubID}'>{$clubName}</a>{$description}</li>";
 }
 
 ?>
