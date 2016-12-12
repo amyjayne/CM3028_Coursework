5908eecb75f71282293824e3b0d1d14d6d2556ce
Jaimie <peanutflea@gmail.com>
2016-12-12 15:25:58 +0000

    update

--- clubs.php
+++ clubs.php
@@ -1,53 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <title>Clubs Page</title>
-    <link rel="stylesheet" href="clubspage.css">
-</head>
+    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="clubs.css">
 
+
+
+</head>
 <body>
 
-<div class="header">
-    <div class="home-menu pure-menu pure-menu-fixed">
-        <ul class="pure-menu-list">
-            <li><a href ="index.php">Home</a></li>
-            <li><a href ="clubs.php">Clubs</a></li>
-            <li><a href ="health.php">Health and Fitness</a></li>
-        </ul>
-    </div>
-</div>
+<div>
 
+
+
+    <header>
+
+
+
+        <div class="header">
+            <div class="home-menu pure-menu pure-menu-fixed">
+                <ul class="pure-menu-list">
+                    <li><a href ="index.html">Home</a></li>
+                    <li><a href ="clubs.html">Clubs</a></li>
+                    <li><a href ="health.html">Health and Fitness</a></li>
+                    <li><a href ="blog.html">Blog</a></li>
+                </ul>
+
+    </header>
+
+</div>
 <div id="banner">
 
 
 
+</div>
+
 
 <div id="content">
+
+
+
+
+
     <div id="basketball">
-        <img src="images/basketball.jpg" id="ball" alt="image">
-        <img src="images/boxing.jpg" id="box" alt="image2">
-        <img src="images/hockey.jpg" id="hock" alt="image3">
+
+
+
+        <img src="images/basketball.jpg">
+
+        <img src="images/boxing.jpg">
+
+        <img src="images/hockey.jpg">
+
+
+
+
+
     </div>
-</div>
+
+
 
 </div>
+
+
+
+
+
+
 </body>
 </html>
-
-<?php
-include("dbconnect.php");
-
-$sql = "SELECT * FROM club";
-$result = $db->query($sql);
-while($row = $result->fetch_array())
-{
-    $clubID = $row['clubID'];
-    $clubName = $row['clubName'];
-    $description = $row['description'];
-    ?>
-    <?
-    echo "<a href='club1.php'>{$clubName}</a>";
-}
-
-?>
